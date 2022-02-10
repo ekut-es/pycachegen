8e116e047ad227036fdc72e66de23bf00872bddf
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2022-02-10 20:06:06 +0100

    Started implementing RegisterFile and its test bench.

--- register_file/RegisterFile.v
+++ register_file/RegisterFile.v
@@ -1,7 +1,47 @@
-module RegisterFile(clk);
-	input clk;
-	always @(posedge clk)
-		begin $display("Hello World!");
-		$finish;
-	end
+module RegisterFile
+#(
+	parameter DATA_WIDTH /* verilator public */ = 32,
+	parameter NUM_REGISTERS /* verilator public */ = 3,
+	parameter READ_WRITE_PORTS /* verilator public */ = 2
+)
+(
+	input clk_i,
+	input reset_n_i,
+	input[0:DATA_WIDTH*READ_WRITE_PORTS*NUM_REGISTERS-1] data_i,
+	input[0:READ_WRITE_PORTS*NUM_REGISTERS-1] write_select_i,
+
+	output[0:DATA_WIDTH*READ_WRITE_PORTS*NUM_REGISTERS-1] data_o
+);
+
+	reg[0:DATA_WIDTH-1] registers[0:NUM_REGISTERS-1];
+
+	generate
+
+		genvar i;
+		genvar j;
+
+		for(i=0; i<NUM_REGISTERS; i=i+1) begin
+			for(j=0; j<READ_WRITE_PORTS; j=j+1) begin
+
+				// example: NUM_REGISTERS = 2, READ_WRITE_PORTS = 2
+				// data_o[0:DATA_WIDTH-1] = registers[0]
+				// data_o[DATA_WIDTH:2*DATA_WIDTH-1] = registers[1]
+				// data_o[2*DATA_WIDTH:3*DATA_WIDTH-1] = registers[0]
+				// data_o[3*DATA_WIDTH:4*DATA_WIDTH-1] = registers[1]
+				//assign data_o[(READ_WRITE_PORTS*i+j)*DATA_WIDTH:(READ_WRITE_PORTS*i+j+1)*DATA_WIDTH-1] = registers[i];
+				assign data_o[(READ_WRITE_PORTS*i+j)*DATA_WIDTH:(READ_WRITE_PORTS*i+j+1)*DATA_WIDTH-1] = registers[i];
+
+				always @(posedge clk_i, negedge reset_n_i) begin
+					if(reset_n_i == 1'b0) begin
+						registers[i] <= 0;
+					end
+					else begin
+						if(write_select_i[READ_WRITE_PORTS*j+i] == 1'b1) begin
+							registers[i] <= data_i[(READ_WRITE_PORTS*i+j)*DATA_WIDTH:(READ_WRITE_PORTS*i+j+1)*DATA_WIDTH-1];
+						end
+					end
+				end
+			end
+		end
+	endgenerate
 endmodule
--- register_file/RegisterFile_tb.cpp
+++ register_file/RegisterFile_tb.cpp
@@ -1,11 +1,81 @@
 #include "VRegisterFile.h"
+#include "verilated_vcd_sc.h"
+
+#include "VRegisterFile_RegisterFile.h"
+#include "VRegisterFile__Dpi.h"
 
 int sc_main(int argc, char** argv) {
     Verilated::commandArgs(argc, argv);
-    sc_clock clk{"clk", 10, SC_NS, 0.5, 3, SC_NS, true};
-    VRegisterFile* top = new VRegisterFile{"top"};
-    top->clk(clk);
-    while (!Verilated::gotFinish()) { sc_start(1, SC_NS); }
-    delete top;
+    Verilated::traceEverOn(true);
+
+    // input signals
+    sc_clock clk{"clk", 1, SC_NS, 0.5, 0, SC_NS, true};
+    sc_signal<bool> reset_n;
+
+    const int parameter_data_width = VRegisterFile_RegisterFile::DATA_WIDTH;
+    const int parameter_num_registers = VRegisterFile_RegisterFile::NUM_REGISTERS;
+    const int parameter_read_write_ports = VRegisterFile_RegisterFile::READ_WRITE_PORTS;
+
+    std::cout << "DATA_WIDTH:       " << parameter_data_width << std::endl;
+    std::cout << "NUM_REGISTERS:    " << parameter_num_registers << std::endl;
+    std::cout << "READ_WRITE_PORTS: " << parameter_read_write_ports << std::endl;
+
+    // CAUTION!!! 
+    // when input/output wire of verilog module has less than 32 bits sc_signal<uint32_t> is inferred
+    // instreand of sc_signal<sc_bv<X>>
+    sc_signal<sc_bv<parameter_data_width*parameter_read_write_ports*parameter_num_registers>> data_i;
+    sc_signal<u_int32_t> write_select_i;
+
+    sc_signal<sc_bv<parameter_data_width*parameter_read_write_ports*parameter_num_registers>> data_o;
+
+    // instantiate verilog module and connect signals
+    VRegisterFile* register_file = new VRegisterFile{"register_file"};
+
+
+    register_file->clk_i(clk);
+    register_file->reset_n_i(reset_n);
+
+    register_file->data_i(data_i);
+    register_file->write_select_i(write_select_i);
+    register_file->data_o(data_o);
+
+    // start simulation, trace, and get parameters
+    sc_start(0, SC_NS);
+
+    sc_start(1, SC_NS);
+
+    VerilatedVcdSc *trace = new VerilatedVcdSc();
+    register_file->trace(trace, 99);
+    trace->open("RegisterFile_tb.vcd");
+
+    // run simulation further
+    reset_n.write(0);
+
+    sc_start(1, SC_NS);
+    reset_n.write(1);
+
+    sc_start(1, SC_NS);
+    reset_n.write(1);
+
+    for(int i = 0; i < parameter_num_registers; i++) {
+        for(int j = 0; j < parameter_read_write_ports; j++) {
+            write_select_i.write(0x1 << parameter_read_write_ports*i+j);
+            data_i.write(0x1 << (parameter_read_write_ports*i+j)*parameter_data_width);
+            sc_start(1, SC_NS);
+            //write_select_i.set_bit(parameter_read_write_ports*i+j, 1);
+        }
+    }
+
+    sc_start(1, SC_NS);
+    register_file->final();
+
+    trace->flush();
+    trace->close();
+
+    delete register_file;
+    delete trace;
+
+    std::cout << "Done!" << std::endl;
+
     return 0;
 }
