7416758183e00a859c6dcb6c4a06b75a5fb77f5f
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2022-07-26 20:38:26 +0200

    Added files to make project installable.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="acaverilog",
+    version="1.0.0",
+    packages=["acaverilog"],
+    package_dir={
+        "acaverilog": "acaverilog"
+    },
+    zip_safe=False
+)
---
+++ test.py
@@ -0,0 +1,9 @@
+DATA_WIDTH = 32
+NUM_REGISTERS = 3
+READ_WRITE_PORTS = 2
+
+print(f"data_i: [0:{DATA_WIDTH*READ_WRITE_PORTS*NUM_REGISTERS-1}]")
+
+for i in range(0, NUM_REGISTERS):
+    for j in range(0, READ_WRITE_PORTS):
+        print(f"i={i},j={j}: [{(READ_WRITE_PORTS*i+j)*DATA_WIDTH}:{(READ_WRITE_PORTS*i+j+1)*DATA_WIDTH-1}]")
