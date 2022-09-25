1a9fe38bb8d476ce79ada37ab928fb1823407c49
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2022-09-25 12:41:46 +0200

    InstructionFetchStage.issue_buffer is filled when new data is available.

--- acaverilog/verilog_templates/instruction_fetch_stage/InstructionFetchStage.v
+++ acaverilog/verilog_templates/instruction_fetch_stage/InstructionFetchStage.v
@@ -33,7 +33,8 @@ module {{ name }}_InstructionFetchStage
 	reg[DATA_WIDTH-1:0] issue_buffer [ISSUE_BUFFER_SIZE-1:0];
 	reg[ISSUE_BUFFER_SIZE-1:0] issue_buffer_valid;
 	wire[ISSUE_BUFFER_SIZE-1:0] issue_buffer_valid_pop_count;
-	wire[$clog2(ISSUE_BUFFER_SIZE):0] issue_buffer_available_slots;
+	wire[ISSUE_BUFFER_SIZE-1:0] issue_buffer_available_slots;
+	integer issue_buffer_first_free_slot;
 
 	// instruction fetch stage only reads from instruction memory
 	assign read_write_select_o = 1'b0;
@@ -57,13 +58,22 @@ module {{ name }}_InstructionFetchStage
 
 	assign issue_buffer_available_slots = ISSUE_BUFFER_SIZE - issue_buffer_valid_pop_count;
 
+	integer i;
+
 	always @(posedge clk_i, negedge reset_n_i) begin
 		if(reset_n_i == 1'b0) begin
-			//address <= {ADDRESS_WIDTH{1'b0}};
 			program_counter <= {{ initial_address }};
 			address_valid <= 1'b0;
 			initialize_read <= 1'b1;
 			read_in_progress <= 1'b0;
+
+			// reset issue buffer
+			for(i = 0; i < ISSUE_BUFFER_SIZE; i = i + 1) begin
+				issue_buffer[i] = {DATA_WIDTH{1'b0}};
+			end
+
+			issue_buffer_valid <= {ISSUE_BUFFER_SIZE{1'b0}};
+			issue_buffer_first_free_slot <= 0;
 		end
 		else begin
 			// initialize a read from memory when instruction memory is ready
@@ -77,10 +87,17 @@ module {{ name }}_InstructionFetchStage
 			if(read_in_progress == 1'b1 && initialize_read == 1'b0 && read_data_valid_i > 0) begin
 				$display("t=%0t: %m read from address=%d with data=%h.", $time, program_counter, read_data_i);
 				read_data <= read_data_i;
+
+				for(i = issue_buffer_first_free_slot; i < issue_buffer_first_free_slot+PORT_WIDTH; i = i + 1) begin
+					issue_buffer[i] = read_data_i[i*DATA_WIDTH +: DATA_WIDTH];
+					issue_buffer_valid[i] <= 1'b1;
+				end
+
+				issue_buffer_first_free_slot <= issue_buffer_first_free_slot + PORT_WIDTH;
+
 				address_valid <= 1'b0;
 				program_counter <= program_counter + PORT_WIDTH;
 				initialize_read <= 1'b1;
-				issue_buffer_valid <= issue_buffer_valid + 1;
 			end
 		end
 	end
