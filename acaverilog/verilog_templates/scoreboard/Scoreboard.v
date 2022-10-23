7ea70a6d6e1ddfa0106f8940f53f3f11b95259aa
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2022-10-23 17:19:52 +0200

    Added instruction_state enum to Scoreboard.

--- acaverilog/verilog_templates/scoreboard/Scoreboard.v
+++ acaverilog/verilog_templates/scoreboard/Scoreboard.v
@@ -13,12 +13,20 @@ module {{ name }}_Scoreboard
 	reg full;
 	assign full_o = full;
 
-	// data structure for scoreboarding
+	typedef enum[1:0] {ISSUE = 0, READ_REGISTERS = 1, EXECUTION = 2, WRITE_REGSISTER = 3} instruction_state;
 
+	// data structure for scoreboarding
+	{%- for i in range(size) %}
+	reg [1:0] instruction_{{ i }}_status;
+	{%- endfor %}
 
 	always @(posedge clk_i, negedge reset_n_i) begin
 		if(reset_n_i == 1'b0) begin
 			full <= 0;
+
+			{%- for i in range(size) %}
+			instruction_{{ i }}_status <= ISSUE;
+			{%- endfor %}
 		end
 		else begin
 		end
