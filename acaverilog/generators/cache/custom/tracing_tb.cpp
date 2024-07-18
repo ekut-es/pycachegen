af339681da1c071246f6d621352628c64b49d218
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-07-18 15:44:38 +0200

    Tracing testbench wastes no more time between requests

--- acaverilog/generators/cache/custom/tracing_tb.cpp
+++ acaverilog/generators/cache/custom/tracing_tb.cpp
@@ -80,8 +80,12 @@ int sc_main(int argc, char** argv) {
 
     const int trace_bytes_per_word = (trace_data_width / 8);
 
-    const int word_buffer_width = 64;
+
+    // Send all instructions to the cache, one after the other
+    const int word_buffer_width = 64; // mem trace is a char array so I first read one trace word into a buffer
+    // the size of that buffer must be known for shifting which I need to do for extracting the address, data, and write enable
     for (uint64_t i = 0; i < mem_trace_bin_len; i += 4) {
+        // send the next instruction
         uint64_t word = 0;
         for (int j = 0; j < trace_bytes_per_word; j++) {
             word |= (mem_trace_bin[i + j] << (trace_data_width - (j + 1) * 8));
@@ -91,17 +95,20 @@ int sc_main(int argc, char** argv) {
         write_data_i.write((word << (word_buffer_width - cache_address_width - cache_data_width)) >> (word_buffer_width - cache_data_width));
         write_data_valid_i.write((word >> (word_buffer_width - 1)));
         read_write_select_i.write((word >> (word_buffer_width - 1)));
+        // wait at least one cycle
         sc_start(1, SC_NS);
-        address_valid_i.write(0);
-        sc_start(1, SC_NS);
+        // as soon as port_ready_o becomes true, the cache will have accepted the request we just sent
+        // so wait until it becomes ready and then proceed with the next request
         while (!port_ready_o.read()) {
             sc_start(1, SC_NS);
         }
+        address_valid_i.write(0);
     }
-    // sc_start(1, SC_NS);
-    // while (!port_ready_o.read()) {
-    //     sc_start(1, SC_NS);
-    // }
+    // wait until the last instruction was processed
+    sc_start(1, SC_NS);
+    while (!port_ready_o.read()) {
+        sc_start(1, SC_NS);
+    }
 
     cache_wrapper->final();
 
@@ -111,7 +118,7 @@ int sc_main(int argc, char** argv) {
     delete trace;
 
     const auto simulated_time =
-        sc_time_stamp().to_default_time_units();
+        sc_time_stamp().to_default_time_units() - 1; // subtract 1 because we wasted one cycle right at the beginning
     const auto end_time = std::chrono::system_clock::now();
     const auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
 
