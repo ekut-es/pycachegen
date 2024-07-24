e73284079b75415dd5b9901fd858e5fd19b603a7
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-07-24 16:33:55 +0200

    CMake now stops on error in generating cache wrappers run_testbenches.sh now reports some more info

--- acaverilog/generators/cache/testbenches/run_testbenches.sh
+++ acaverilog/generators/cache/testbenches/run_testbenches.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+echo "Running testbenches..."
+
 failed_testbenches=()
+num_testbenches="0"
 
 for filename in Vcache_wrapper_tb_*; do
     if [[ ! $filename ==  *.vcd ]] ; then
-        # echo $filename
+        let "num_testbenches++"
         eval "./$filename" > /dev/null 2>&1
         return_value=$?
         if [ $return_value -ne 0 ]; then
@@ -13,11 +16,13 @@ for filename in Vcache_wrapper_tb_*; do
     fi
 done
 
-if [ ${#failed_testbenches[@]} -ne 0 ]; then
-    echo "Some testbenches failed:"
+num_failed_testbenches=${#failed_testbenches[@]}
+
+if [ ${num_failed_testbenches} -ne 0 ]; then
+    echo "${num_failed_testbenches} out of ${num_testbenches} testbenches failed:"
     for failed_filename in ${failed_testbenches[@]}; do
         echo ${failed_filename}
     done
 else
-    echo "All testbenches finished without errors"
+    echo "Ran ${num_testbenches} testbenches without errors"
 fi
