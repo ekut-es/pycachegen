2da52e65b2761da6f4f09acd907b1fe1e53fdd79
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-07-30 14:45:47 +0200

    Removed test function

--- src/pycachegen/__init__.py
+++ src/pycachegen/__init__.py
@@ -1,4 +1,3 @@
-from .pycachegen import test_func
 from pycachegen.generators.cache_wrapper_generator import CacheWrapperGenerator
 from pycachegen.generators.cache_generator import CacheGenerator
 from pycachegen.cache_config_validation import MemoryConfig, CacheConfig
