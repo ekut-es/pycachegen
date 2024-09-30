755f5b77a86ccd8d0bb889648ace3a530a7f384a
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-09-30 14:43:46 +0200

    Added a custom Amaranth Memory module

---
+++ src/pycachegen/amaranth/main_memory.py
@@ -0,0 +1,179 @@
+from amaranth import *
+from amaranth.lib import wiring, data
+from amaranth.lib.memory import Memory
+from amaranth.lib.wiring import In, Out
+from amaranth.sim import Simulator
+from pycachegen.cache_config_validation import InternalMemoryConfig, MemoryConfig
+
+
+class MemoryBusSignature(wiring.Signature):
+    def __init__(self, address_width: int, data_width: int, bytes_per_word: int):
+        self.address_width = address_width
+        self.data_width = data_width
+        self.bytes_per_word = bytes_per_word
+        super().__init__(
+            [
+                ("address", Out(address_width)),
+                ("write_data", Out(data_width)),
+                ("write_strobe", Out(bytes_per_word)),
+                ("request_valid", Out(1)),
+                ("read_data", In(data_width)),
+                ("read_data_valid", In(1)),
+                ("port_ready", In(1)),
+            ]
+        )
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, MemoryBusSignature)
+            and self.address_width == other.address_width
+            and self.data_width == other.data_width
+            and self.bytes_per_word == other.bytes_per_word
+        )
+
+    def __repr__(self):
+        return f"MemoryBusSignature({self.address_width}, {self.data_width}, {self.bytes_per_word})"
+
+    def create(self, *, path=None, src_loc_at=0):
+        return MemoryBusInterface(self, path=path, src_loc_at=1 + src_loc_at)
+
+
+class MemoryBusInterface(wiring.PureInterface):
+    def is_read_request(self):
+        return self.request_valid & (self.write_strobe == 0)
+
+    def is_write_request(self):
+        return self.request_valid & (self.write_strobe != 0)
+
+
+class MainMemory(wiring.Component):
+    def __init__(self, config: InternalMemoryConfig) -> None:
+        self.config = config
+        self.bytes_per_word = config.DATA_WIDTH // config.BYTE_SIZE
+
+        super().__init__(
+            {
+                "mem_if": In(
+                    MemoryBusSignature(
+                        address_width=config.ADDRESS_WIDTH,
+                        data_width=config.DATA_WIDTH,
+                        bytes_per_word=self.bytes_per_word,
+                    )
+                )
+            }
+        )
+
+    def elaborate(self, platform) -> Module:
+        m = Module()
+
+        processing_request = Signal(1)  # whether a request is currently being processed
+        m.d.comb += self.mem_if.port_ready.eq(~processing_request)
+        request_type = Signal(1)  # type of current request (read: 0, write: 1)
+        latency_counter = Signal(range(max(self.config.READ_LATENCY, self.config.WRITE_LATENCY)))
+        # align address for Amaranth Memory with specified memory range
+        aligned_address = Signal(range(0, self.config.MAX_ADDRESS - self.config.MIN_ADDRESS))
+        m.d.comb += aligned_address.eq(self.mem_if.address - self.config.MIN_ADDRESS)
+
+        m.submodules.data_memory = data_memory = Memory(
+            shape=unsigned(self.config.DATA_WIDTH),
+            depth=(self.config.MAX_ADDRESS - self.config.MIN_ADDRESS),
+            init=[],
+        )
+
+        write_port = data_memory.write_port(granularity=self.config.BYTE_SIZE)
+        m.d.comb += write_port.addr.eq(aligned_address)
+        m.d.comb += write_port.data.eq(self.mem_if.write_data)
+
+        read_port = data_memory.read_port()
+        m.d.comb += read_port.addr.eq(aligned_address)
+        m.d.comb += self.mem_if.read_data.eq(read_port.data)
+
+        with m.If(~processing_request):
+            with m.If(
+                self.mem_if.request_valid
+                & (self.mem_if.address >= self.config.MIN_ADDRESS)
+                & (self.mem_if.address < self.config.MAX_ADDRESS)
+            ):
+                # Buffer inputs
+                m.d.sync += processing_request.eq(1)
+                m.d.sync += request_type.eq(self.mem_if.write_strobe.any())
+
+                # Reset response
+                m.d.sync += self.mem_if.read_data_valid.eq(0)
+
+                # Increment latency counter
+                m.d.sync += latency_counter.eq(latency_counter + 1)
+
+                # Initiate read/write
+                m.d.sync += write_port.en.eq(self.mem_if.write_strobe)
+                m.d.sync += read_port.en.eq(~self.mem_if.write_strobe.any())
+        with m.Else():
+            # clear enable on read/write ports
+            m.d.sync += read_port.en.eq(0)
+            m.d.sync += write_port.en.eq(0)
+            with m.If(
+                ((request_type == 0) & (latency_counter == self.config.READ_LATENCY - 1))
+                | ((request_type == 1) & (latency_counter == self.config.WRITE_LATENCY - 1))
+            ):
+                # Reset internal registers
+                m.d.sync += processing_request.eq(0)
+                m.d.sync += latency_counter.eq(0)
+                # Set correct response
+                m.d.sync += self.mem_if.read_data_valid.eq(~request_type)
+            with m.Else():
+                m.d.sync += latency_counter.eq(latency_counter + 1)
+
+        return m
+
+
+if __name__ == "__main__":
+    dut = MainMemory(
+        config=InternalMemoryConfig(
+            MemoryConfig(
+                data_width=32,
+                read_latency=2,
+                write_latency=3,
+                min_address=(3 << 2),
+                max_address=(7 << 2),
+            ),
+            address_width=3,
+            byte_size=8,
+            enable_reset=True,
+        )
+    )
+
+    async def bench(ctx):
+        # write
+        ctx.set(dut.mem_if.address, 5)
+        ctx.set(dut.mem_if.write_strobe, 0b1101)
+        ctx.set(dut.mem_if.write_data, 0xA0A1A2A3)
+        ctx.set(dut.mem_if.request_valid, 1)
+
+        # wait
+        await ctx.tick()
+        assert not ctx.get(dut.mem_if.port_ready)
+        ctx.set(dut.mem_if.request_valid, 0)
+        await ctx.tick()
+        assert not ctx.get(dut.mem_if.port_ready)
+
+        # read
+        await ctx.tick()
+        assert ctx.get(dut.mem_if.port_ready)
+        ctx.set(dut.mem_if.write_strobe, 0)
+        ctx.set(dut.mem_if.request_valid, 1)
+
+        # wait
+        await ctx.tick()
+        assert not ctx.get(dut.mem_if.port_ready)
+        ctx.set(dut.mem_if.request_valid, 0)
+
+        await ctx.tick()
+        assert ctx.get(dut.mem_if.port_ready)
+        assert ctx.get(dut.mem_if.read_data_valid)
+        assert ctx.get(dut.mem_if.read_data == 0xA0A100A3)
+        
+    sim = Simulator(dut)
+    sim.add_clock(1e-6)
+    sim.add_testbench(bench)
+    with sim.write_vcd("main_memory.vcd"):
+        sim.run()
