c77b2ce31ecca98f20956a0da1c97f9b759e95ec
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-10-02 17:43:32 +0200

    Did more work on the Amaranth cache

--- src/pycachegen/amaranth/cache.py
+++ src/pycachegen/amaranth/cache.py
@@ -83,12 +83,14 @@ class Cache(wiring.Component):
         # purely for statistics: let the outside know if we had a hit
         m.d.comb += self.hit_o.eq(hit_vector.any())
         # one hot encode the vector into this signal
-        hit_index = Signal(unsigned(self.index_width))
+        hit_index = Signal(unsigned(range(self.config.NUM_WAYS)))
         m.submodules.hit_index_encoder = hit_index_encoder = coding.Encoder(hit_vector.shape().width)
         m.d.comb += hit_index_encoder.i.eq(hit_vector)
         m.d.comb += hit_index.eq(hit_index_encoder.o)
         # a view for convenient access to the tag/index/word offset bits
         fe_address_view = data.View(self.fe_signature, self.fe.address)
+        # block to be replaced next
+        next_block_replacement = Signal(unsigned(range(self.config.NUM_WAYS)))
 
         # Create be buffers that use our own data/address/write strobe widths
         # Then "shift" the data/write strobe into place in the real be interface
@@ -132,7 +134,35 @@ class Cache(wiring.Component):
                     new_data_mem.write_port(granularity=self.config.BYTE_SIZE),
                 )
             )
+            # disable data_mem.read_port.en by default so that the read data
+            # gets preserved even when the address changes
+            # thats necessary because the read data might only be needed
+            # after several cycles
+            m.d.comb += data_mem[-1][0].en.eq(0)
 
+        ## things for write back
+        # address and way to identify the block to be written back
+        write_back_address = Signal(
+            CacheAddressLayout(
+                index_width=self.index_width,
+                tag_width=self.tag_width,
+                word_offset_width=self.word_offset_width,
+            )
+        )
+        write_back_way = Signal(unsigned(range(self.config.NUM_WAYS)))
+        # state to take after the WRITE_BACK state
+        write_back_next_state = Signal(States)
+
+
+        def send_buffered_request_to_lower_mem(next_state: States):
+            """Uses the buffered inputs to send a request to the lower memory.
+            Goes to the SEND_MEM_REQUEST state to do so.
+            """
+            m.d.sync += be_buffer_address.eq(fe_buffer_address)
+            m.d.sync += be_buffer_write_data.eq(fe_buffer_write_data)
+            m.d.sync += be_buffer_write_strobe.eq(fe_buffer_write_strobe)
+            m.d.sync += state.eq(States.SEND_MEM_REQUEST)
+            m.d.sync += send_mem_request_next_state.eq(next_state)
 
         with m.Switch(state):
             with m.Case(States.READY):
@@ -171,24 +201,74 @@ class Cache(wiring.Component):
                     with m.If(fe_buffer_write_strobe.any()):
                         # handle write
                         m.d.comb += data_mem[hit_index][1].en.eq(fe_buffer_write_strobe)
-                        m.d.comb += data_mem[hit_index][1].address.eq(Cat(fe_address_view.word_offset, fe_address_view.index))
+                        m.d.comb += data_mem[hit_index][1].addr.eq(Cat(fe_address_view.word_offset, fe_address_view.index))
                         m.d.comb += data_mem[hit_index][1].data.eq(fe_buffer_write_data)
                         if self.config.WRITE_BACK:
                             # mark dirty if write back
                             m.d.comb += dirty_mem[hit_index][1].en.eq(1)
-                            m.d.comb += dirty_mem[hit_index][1].address.eq(fe_address_view.index)
+                            m.d.comb += dirty_mem[hit_index][1].addr.eq(fe_address_view.index)
                             m.d.comb += dirty_mem[hit_index][1].data.eq(1)
                             m.d.sync += state.eq(States.STALL)
                         else:
                             # write to lower mem if write through
-                            m.d.sync += be_buffer_address.eq(fe_buffer_address)
-                            m.d.sync += be_buffer_write_data.eq(fe_buffer_write_data)
-                            m.d.sync += be_buffer_write_strobe.eq(fe_buffer_write_strobe)
-                            m.d.sync += state.eq(States.SEND_MEM_REQUEST)
-                            m.d.sync += send_mem_request_next_state.eq(States.STALL)
+                            send_buffered_request_to_lower_mem(next_state=States.STALL)
                     with m.Else():
-                        # TODO Handle read
-                        ...
+                        # handle read
+                        m.d.sync += state.eq(States.STALL)
+                        # FIXME read not initiatet, .data forgotten
+                        self.fe.read_data.eq(data_mem[hit_index][Cat(fe_address_view.word_offset, fe_address_view.index)])
                 with m.Else():
-                    # TODO Handle miss
-                    ...
+                    # Handle miss
+                    with m.If((not self.config.WRITE_ALLOCATE) & fe_buffer_write_strobe.any()):
+                        # write miss and write no-allocate -> only write to lower memory
+                        send_buffered_request_to_lower_mem(next_state=States.STALL)
+                    with m.Else():
+                        # It's a miss and we have to replace a block
+
+                        # determine the state that will be taken next (or after write back)
+                        # If it's a read or if not all bytes of the block are being written to
+                        # (because the write strobe is not all ones or becaues there is more than
+                        # one word in the block) we need to perform a read block operation afterwards
+                        read_block_operation_needed = (~fe_buffer_write_strobe).any() | (self.config.BLOCK_SIZE > 1)
+                        next_state = Mux(read_block_operation_needed, States.READ_BLOCK, States.READ_BLOCK_DONE)
+                        # Update valid/dirty/tag memories
+                        m.d.comb += valid_mem[hit_index][1].en.eq(1)
+                        m.d.comb += valid_mem[hit_index][1].addr.eq(fe_buffer_address.index)
+                        m.d.comb += valid_mem[hit_index][1].data.eq(1)
+                        m.d.comb += tag_mem[hit_index][1].en.eq(1)
+                        m.d.comb += tag_mem[hit_index][1].addr.eq(fe_buffer_address.index)
+                        m.d.comb += tag_mem[hit_index][1].data.eq(fe_buffer_address.tag)
+                        if self.config.WRITE_BACK:
+                            m.d.comb += dirty_mem[hit_index][1].en.eq(1)
+                            m.d.comb += dirty_mem[hit_index][1].addr.eq(fe_buffer_address.index)
+                            m.d.comb += dirty_mem[hit_index][1].data.eq(1)
+                        # TODO update the replacement policy
+                        # TODO initiate read from dirty memories in the previous state
+                        with m.If(self.config.WRITE_BACK & dirty_mem[next_block_replacement][0].data):
+                            # We have to write back the block to be replaced first because its dirty
+                            m.d.sync += state.eq(States.WRITE_BACK_BLOCK)
+                            m.d.sync += write_back_address.eq(fe_buffer_address)
+                            m.d.sync += write_back_address.word_offset.eq(0) # start at the first word
+                            m.d.sync += write_back_way.eq(next_block_replacement)
+                            m.d.sync += write_back_next_state.eq(next_state)
+                        with m.Else():
+                            # No write back needed
+                            m.d.sync += state.eq(next_state)
+            with m.Case(States.WRITE_BACK_BLOCK):
+                # Write back the block specified by the respective registers
+                # initiate a read from the data memory. tell the SEND_MEM_REQUEST
+                # state that it should use the data from there
+                m.d.comb += data_mem[write_back_way][0].en.eq(1)
+                m.d.comb += data_mem[write_back_way][0].address.eq(write_back_address)
+                m.d.sync += latency_counter.eq(latency_counter + 1)
+                m.d.sync += be_buffer_address.eq(write_back_address)
+                m.d.sync += be_buffer_write_strobe.eq(-1) # NOTE This might not work
+                m.d.sync += state(States.SEND_MEM_REQUEST)
+                with m.If(write_back_address.word_offset == (self.config.BLOCK_SIZE - 1)):
+                    # This is the last word to write back
+                    m.d.sync += send_mem_request_next_state.eq(write_back_next_state)
+                with m.Else():
+                    # We're not done yet, come back after mem request has finished
+                    m.d.sync += send_mem_request_next_state.eq(States.WRITE_BACK_BLOCK)
+            with m.Case(States.SEND_MEM_REQUEST):
+                ...
