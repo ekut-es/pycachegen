fa2aee16188e948c714eb79a73befc0343d8c760
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-10-11 10:38:25 +0200

    Added pytest for executing all testbenches at once

--- src/pycachegen/amaranth/testbenches/cache_wrapper_tb_1.py
+++ src/pycachegen/amaranth/testbenches/cache_wrapper_tb_1.py
@@ -5,52 +5,55 @@ from pycachegen.amaranth.testbenches.tb_utils import (
 from pycachegen.amaranth.cache_wrapper import CacheWrapper
 from pycachegen.cache_config_validation import CacheConfig, MemoryConfig
 
+
 # Testbench for testing a direct mapped cache
-
-dut = CacheWrapper(
-    num_ports=1,
-    arbiter_policy="priority",
-    byte_size=8,
-    enable_reset=False,
-    address_width=8,
-    cache_configs=[
-        CacheConfig(
+def test():
+    dut = CacheWrapper(
+        num_ports=1,
+        arbiter_policy="priority",
+        byte_size=8,
+        enable_reset=False,
+        address_width=8,
+        cache_configs=[
+            CacheConfig(
+                data_width=16,
+                num_ways=1,
+                num_sets=4,
+                replacement_policy="fifo",
+                hit_latency=10,
+                miss_latency=15,
+                write_through=True,
+                write_allocate=False,
+                block_size=1,
+            )
+        ],
+        memory_config=MemoryConfig(
             data_width=16,
-            num_ways=1,
-            num_sets=4,
-            replacement_policy="fifo",
-            hit_latency=10,
-            miss_latency=15,
-            write_through=True,
-            write_allocate=False,
-            block_size=1,
-        )
-    ],
-    memory_config=MemoryConfig(
-        data_width=16, read_latency=10, write_latency=15, min_address=0, max_address=512
-    ),
-)
+            read_latency=10,
+            write_latency=15,
+            min_address=0,
+            max_address=512,
+        ),
+    )
 
-helper = CacheWrapperBenchHelper(dut)
+    helper = CacheWrapperBenchHelper(dut)
 
+    async def bench(ctx):
+        await helper.read(ctx, 0, 0, False)
+        await helper.read(ctx, 1, 0, False)
+        await helper.read(ctx, 2, 0, False)
+        await helper.read(ctx, 3, 0, False)
 
-async def bench(ctx):
-    await helper.read(ctx, 0, 0, False)
-    await helper.read(ctx, 1, 0, False)
-    await helper.read(ctx, 2, 0, False)
-    await helper.read(ctx, 3, 0, False)
+        await helper.read(ctx, 0, 0, True)
+        await helper.read(ctx, 1, 0, True)
+        await helper.read(ctx, 2, 0, True)
+        await helper.read(ctx, 3, 0, True)
 
-    await helper.read(ctx, 0, 0, True)
-    await helper.read(ctx, 1, 0, True)
-    await helper.read(ctx, 2, 0, True)
-    await helper.read(ctx, 3, 0, True)
+        await helper.write(ctx, 4, 333, False)
+        await helper.read(ctx, 0, 0, True)
+        await helper.read(ctx, 4, 333, False)
+        await helper.read(ctx, 4, 333, True)
+        await helper.read(ctx, 0, 0, False)
+        await helper.read(ctx, 0, 0, True)
 
-    await helper.write(ctx, 4, 333, False)
-    await helper.read(ctx, 0, 0, True)
-    await helper.read(ctx, 4, 333, False)
-    await helper.read(ctx, 4, 333, True)
-    await helper.read(ctx, 0, 0, False)
-    await helper.read(ctx, 0, 0, True)
-
-
-run_bench(dut=dut, bench=bench)
+    run_bench(dut=dut, bench=bench)
--- src/pycachegen/amaranth/testbenches/cache_wrapper_tb_4.py
+++ src/pycachegen/amaranth/testbenches/cache_wrapper_tb_4.py
@@ -5,50 +5,53 @@ from pycachegen.amaranth.testbenches.tb_utils import (
 from pycachegen.amaranth.cache_wrapper import CacheWrapper
 from pycachegen.cache_config_validation import CacheConfig, MemoryConfig
 
+
 # Testbench for testing the plru_tree replacement policy
-
-dut = CacheWrapper(
-    num_ports=1,
-    arbiter_policy="priority",
-    byte_size=8,
-    enable_reset=False,
-    address_width=8,
-    cache_configs=[
-        CacheConfig(
+def test():
+    dut = CacheWrapper(
+        num_ports=1,
+        arbiter_policy="priority",
+        byte_size=8,
+        enable_reset=False,
+        address_width=8,
+        cache_configs=[
+            CacheConfig(
+                data_width=16,
+                num_ways=4,
+                num_sets=2,
+                replacement_policy="plru_tree",
+                hit_latency=10,
+                miss_latency=15,
+                write_through=True,
+                write_allocate=False,
+                block_size=1,
+            )
+        ],
+        memory_config=MemoryConfig(
             data_width=16,
-            num_ways=4,
-            num_sets=2,
-            replacement_policy="plru_tree",
-            hit_latency=10,
-            miss_latency=15,
-            write_through=True,
-            write_allocate=False,
-            block_size=1,
-        )
-    ],
-    memory_config=MemoryConfig(
-        data_width=16, read_latency=10, write_latency=15, min_address=0, max_address=512
-    ),
-)
+            read_latency=10,
+            write_latency=15,
+            min_address=0,
+            max_address=512,
+        ),
+    )
 
-helper = CacheWrapperBenchHelper(dut)
+    helper = CacheWrapperBenchHelper(dut)
 
+    async def bench(ctx):
+        # Read from 0, 2, 4, 6 (all go into the same set)
+        await helper.read(ctx, 0, 0, False)
+        await helper.read(ctx, 2, 0, False)
+        await helper.read(ctx, 0, 0, True)
+        await helper.read(ctx, 4, 0, False)
+        await helper.read(ctx, 6, 0, False)
+        await helper.read(ctx, 4, 0, True)
+        await helper.read(ctx, 8, 0, False)
+        await helper.read(ctx, 4, 0, True)
+        await helper.read(ctx, 2, 0, True)
+        await helper.read(ctx, 8, 0, True)
+        await helper.read(ctx, 0, 0, False)
+        await helper.read(ctx, 2, 0, True)
+        await helper.read(ctx, 4, 0, False)
 
-async def bench(ctx):
-    # Read from 0, 2, 4, 6 (all go into the same set)
-    await helper.read(ctx, 0, 0, False)
-    await helper.read(ctx, 2, 0, False)
-    await helper.read(ctx, 0, 0, True)
-    await helper.read(ctx, 4, 0, False)
-    await helper.read(ctx, 6, 0, False)
-    await helper.read(ctx, 4, 0, True)
-    await helper.read(ctx, 8, 0, False)
-    await helper.read(ctx, 4, 0, True)
-    await helper.read(ctx, 2, 0, True)
-    await helper.read(ctx, 8, 0, True)
-    await helper.read(ctx, 0, 0, False)
-    await helper.read(ctx, 2, 0, True)
-    await helper.read(ctx, 4, 0, False)
-
-
-run_bench(dut=dut, bench=bench)
+    run_bench(dut=dut, bench=bench)
