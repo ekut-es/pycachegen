80f213a94578b039d2f998bf255444f949740328
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-10-13 13:13:30 +0200

    Added a testbench for generating cache wrappers without caches

---
+++ src/pycachegen/amaranth/testbenches/cache_wrapper_tb_24.py
@@ -0,0 +1,44 @@
+from pycachegen.amaranth.testbenches.tb_utils import (
+    CacheWrapperBenchHelper,
+    run_bench,
+)
+from pycachegen.amaranth.cache_wrapper import CacheWrapper
+from pycachegen.cache_config_validation import CacheConfig, MemoryConfig
+
+
+# Testbench for testing that the cache wrapper can generate architectures without caches
+def test():
+    dut = CacheWrapper(
+        num_ports=1,
+        arbiter_policy="priority",
+        byte_size=8,
+        enable_reset=False,
+        address_width=3,
+        cache_configs=[],
+        memory_config=MemoryConfig(
+            data_width=16,
+            read_latency=10,
+            write_latency=15,
+            min_address=0,
+            max_address=16,
+        ),
+    )
+
+    helper = CacheWrapperBenchHelper(dut)
+
+    async def bench(ctx):
+        # just do a bit of reading and writing
+        await helper.read(ctx, 0, 0, False)
+        await helper.read(ctx, 1, 0, False)
+        await helper.read(ctx, 2, 0, False)
+        await helper.read(ctx, 3, 0, False)
+
+        await helper.write(ctx, 4, 0x1040, False)
+        await helper.read(ctx, 4, 0x1040, False)
+        await helper.read(ctx, 0, 0, False)
+
+    run_bench(dut=dut, bench=bench)
+
+
+if __name__ == "__main__":
+    test()
