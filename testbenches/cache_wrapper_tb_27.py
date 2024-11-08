167aae43fe8569edde8ea800529491fc831f3652
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-11-08 12:11:18 +0100

    Added a testbench for delayed read data handout

---
+++ testbenches/cache_wrapper_tb_27.py
@@ -0,0 +1,76 @@
+from .tb_utils import (
+    CacheWrapperBenchHelper,
+    run_bench,
+)
+from pycachegen.cache_wrapper import CacheWrapper
+from pycachegen.cache_config_validation import (
+    CacheConfig,
+    MemoryConfig,
+    ReplacementPolicies,
+)
+
+
+# Testbench for testing that handing out read data before ready
+# gets delayed properly
+def test():
+    hit_latency = 5
+    miss_latency = 16
+    dut = CacheWrapper(
+        num_ports=1,
+        arbiter_policy="priority",
+        byte_size=8,
+        enable_reset=False,
+        address_width=8,
+        cache_configs=[
+            CacheConfig(
+                data_width=16,
+                num_ways=2,
+                num_sets=2,
+                replacement_policy=ReplacementPolicies.LRU,
+                write_through=False,
+                write_allocate=True,
+                block_size=4,
+                hit_latency=hit_latency,
+                miss_latency=miss_latency,
+            ),
+        ],
+        memory_config=MemoryConfig(
+            data_width=32,
+            read_latency=5,
+            write_latency=8,
+            min_address=0,
+            max_address=128,
+        ),
+    )
+
+    helper = CacheWrapperBenchHelper(dut)
+
+    async def bench(ctx):
+        # Create a read miss
+        ctx.set(dut.fe.request_valid, 1)
+        ctx.set(dut.fe.address, 0)
+        ctx.set(dut.fe.write_strobe, 0)
+
+        # wait until the read data is valid
+        while not ctx.get(dut.fe.read_data_valid):
+            await ctx.tick()
+
+        # check that the port is not yet ready
+        assert not ctx.get(dut.fe.port_ready)
+
+        while not ctx.get(dut.fe.port_ready):
+            await ctx.tick()
+
+        # now do some other reads/writes (without timing checks)
+        # to check that everything still works
+        await helper.read(ctx, 0, 0, True)
+        await helper.read(ctx, 1, 0, True)
+        await helper.write(ctx, 2, 0x1002, True)
+        await helper.read(ctx, 2, 0x1002, True)
+        await helper.read(ctx, 3, 0, True)
+
+    run_bench(dut=dut, bench=bench)
+
+
+if __name__ == "__main__":
+    test()
