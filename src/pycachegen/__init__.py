d2ee2c0684842e20b2d48cd946cf4517248c2176
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-11-15 12:26:34 +0100

    Updated the top level __init__

--- src/pycachegen/__init__.py
+++ src/pycachegen/__init__.py
@@ -1,5 +1,7 @@
 from pycachegen.cache_wrapper import CacheWrapper
 from pycachegen.cache import Cache
 from pycachegen.cache_config_validation import MemoryConfig, CacheConfig
+from pycachegen.arbiter import ArbitrationScheme
+from pycachegen.replacement_policy import ReplacementPolicies
 
 __version__ = "0.1.0"
