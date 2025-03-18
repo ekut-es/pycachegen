237f7ee9c6a6b539a5212b5b1171942d2f699711
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2025-03-18 11:48:20 +0100

    Refactored the pulpissimo wrapper

--- src/pycachegen/pulpissimo/pulpissimo_cache_subsystem_wrapper.sv
+++ src/pycachegen/pulpissimo/pulpissimo_cache_subsystem_wrapper.sv
@@ -1,3 +1,11 @@
+// Module which integrates a data cache into the pulpissimo SoC.
+// This Module takes all TCDM requesters as inputs on tcdm_in. Requests that are addressed for private bank 0
+// (which is where all data and nothing else should be) are then arbitrated and sent to the cache.
+// If your linker script works differently, you might get poor performance.
+// The cache's own request interface is output as tcdm_priv0. Since all requests for private bank 0
+// will come from this tcdm_priv0, the TCDM Demux should not get a rule for this address range and its
+// output port should be replaced by tcdm_priv0.
+// Requests for other addresses bypass the cache and will simply be output again on tcdm_out.
 module pulpissimo_cache_subsystem_wrapper #(
   parameter int NR_MASTER_PORTS = 4,
   parameter int PRIV_BANK_0_MIN_ADDR = 32'h1C00_0000,
@@ -12,111 +20,78 @@ module pulpissimo_cache_subsystem_wrapper #(
   localparam int unsigned BE_WIDTH = 4;
   localparam int unsigned ADDR_WIDTH = 32;
   localparam int unsigned DATA_WIDTH = 32;
-  localparam int unsigned REQ_AGG_DATA_WIDTH  = 1+BE_WIDTH+ADDR_WIDTH+DATA_WIDTH; // +1 is for the write enable (wen),
+  localparam int unsigned REQ_AGG_DATA_WIDTH  = 1+BE_WIDTH+ADDR_WIDTH+DATA_WIDTH; // +1 is for wen
 
-  // Response status of each master port:
-  //  0: No ouststanding responses
-  //  1: Outstanding response from cache
-  //  2: Outstanding response from memory
-  logic [1:0] outstanding_response [NR_MASTER_PORTS-1:0];
-  logic [NR_MASTER_PORTS-1:0] arb_masters_gnt;
+  logic outstanding_cache_response; // Whether we're awaiting a response from the cache
   
-  //Cache Data Bus (Arbiter<->Cache Subsystem)
+  // indices of the next request (as outputted by the arbiter) and of the previously granted request
+  logic [$clog2(NR_MASTER_PORTS)-1:0] arb_next_idx;
+  logic [$clog2(NR_MASTER_PORTS)-1:0] arb_curr_idx;
+
+  // (Requesters<->Arbiter)
+  logic [NR_MASTER_PORTS-1:0] req_2_arb_gnt;
+  logic [NR_MASTER_PORTS-1:0] req_2_arb_req;
+  logic [NR_MASTER_PORTS-1:0][REQ_AGG_DATA_WIDTH-1:0] req_2_arb_data_agg; // Aggreagate the input data for the arbiter
+  for (genvar i = 0; i < NR_MASTER_PORTS; i++) begin
+    assign req_2_arb_data_agg[i] = {tcdm_in[i].wen, tcdm_in[i].be, tcdm_in[i].add, tcdm_in[i].wdata};
+  end
+
+  // (Arbiter<->Cache Subsystem)
   logic [31:0] arb_2_cache_addr, arb_2_cache_rdata, arb_2_cache_wdata;
   logic        arb_2_cache_req, arb_2_cache_gnt, arb_2_cache_rvalid, arb_2_cache_err;
   logic        arb_2_cache_wen;
-  logic [ 3:0]  arb_2_cache_be;
-      
-  logic [NR_MASTER_PORTS-1:0] arbiter_req_in;
-  logic [NR_MASTER_PORTS-1:0] new_request_allowed;
-  logic [NR_MASTER_PORTS-1:0] address_for_cache;
+  logic [ 3:0] arb_2_cache_be;
+  logic [REQ_AGG_DATA_WIDTH-1:0] arb_2_cache_data_agg; // Disaggregate the output data from the arbiter
+  assign {arb_2_cache_wen, arb_2_cache_be, arb_2_cache_addr, arb_2_cache_wdata} = arb_2_cache_data_agg;
 
-  // update outstanding_response status
-  for (genvar i = 0; i < NR_MASTER_PORTS; i++) begin
-    always_ff @(posedge clk_i, negedge rst_ni) begin
-      if (!rst_ni) begin
-        outstanding_response[i] <= 2'd0;
-      end else if (arbiter_req_in[i] && arb_masters_gnt[i]) begin
-        outstanding_response[i] <= 2'd1; // new granted request for cache
-      end else if (tcdm_out[i].req && tcdm_out[i].gnt) begin
-        outstanding_response[i] <= 2'd2; // new granted request for memory
-      end else if ((outstanding_response[i] == 2'd1 && arb_2_cache_rvalid) || (outstanding_response[i] == 2'd2 && tcdm_out[i].r_valid)) begin
-        outstanding_response[i] <= 2'd0; // response but no new granted request
+
+  // update outstanding_cache_response status
+  always_ff @(posedge clk_i, negedge rst_ni) begin
+    if (!rst_ni) begin
+      outstanding_cache_response <= 1'b0;
+    end else begin
+      if (arb_2_cache_req && arb_2_cache_gnt) begin
+        outstanding_cache_response <= 1'b1; // new granted request for cache
+      end else if (outstanding_cache_response == 1'b1 && arb_2_cache_rvalid) begin
+        outstanding_cache_response <= 1'b0; // response but no new granted request
       end else begin
-        outstanding_response[i] <= outstanding_response[i]; // no response
+        outstanding_cache_response <= outstanding_cache_response; // no new request or response
       end
     end
   end
 
-  // forward the responses back to the correct requesters
+  // Request & Response routing to/from arbiter and regular outputs
   for (genvar i = 0; i < NR_MASTER_PORTS; i++) begin
     always_comb begin
-      case (outstanding_response[i])
-        1: begin
-          tcdm_in[i].r_valid = arb_2_cache_rvalid;
-          tcdm_in[i].r_rdata = arb_2_cache_rdata;
-          tcdm_in[i].r_opc = arb_2_cache_err;
-        end
-        2: begin
-          tcdm_in[i].r_valid = tcdm_out[i].r_valid;
-          tcdm_in[i].r_rdata = tcdm_out[i].r_rdata;
-          tcdm_in[i].r_opc = tcdm_out[i].r_opc;
-        end
-        default:  begin
-          tcdm_in[i].r_valid = 0;
-          tcdm_in[i].r_rdata = 0;
-          tcdm_in[i].r_opc = 0;
-        end
-      endcase
-    end
-  end
-
-  for (genvar i = 0; i < NR_MASTER_PORTS; i++) begin
-    always_comb begin
-      // always forward the request signals (except for req) to the outputs
+      // Check whether the req has to go to cache or memory. Also send the gnt signal back correctly.
+      if ((tcdm_in[i].add >= PRIV_BANK_0_MIN_ADDR) && (tcdm_in[i].add < PRIV_BANK_0_MAX_ADDR)) begin
+        req_2_arb_req[i] = tcdm_in[i].req;
+        tcdm_out[i].req = 0;
+        tcdm_in[i].gnt = req_2_arb_gnt[i];
+      end else begin
+        req_2_arb_req[i] = 0;
+        tcdm_out[i].req = tcdm_in[i].req;
+        tcdm_in[i].gnt = tcdm_out[i].gnt;
+      end
+      // Always forward all the other the request signals to the regular outputs
       tcdm_out[i].add = tcdm_in[i].add;
       tcdm_out[i].wdata = tcdm_in[i].wdata;
       tcdm_out[i].wen = tcdm_in[i].wen;
       tcdm_out[i].be = tcdm_in[i].be;
-      // New requests are allowed if there is no outstanding response or if there is a response
-      new_request_allowed[i] = (!outstanding_response[i] || (outstanding_response[i] == 1 && arb_2_cache_rvalid) || (outstanding_response[i] == 2 && tcdm_out[i].r_valid));
-      // If we can send a new request, check where it has to go to (cache or memory).
-      // Also send the gnt signal back correctly.
-      address_for_cache[i] = (tcdm_in[i].add >= PRIV_BANK_0_MIN_ADDR) && (tcdm_in[i].add < PRIV_BANK_0_MAX_ADDR);
-      if (new_request_allowed[i]) begin
-        if (address_for_cache[i]) begin
-          arbiter_req_in[i] = tcdm_in[i].req;
-          tcdm_out[i].req = 0;
-          tcdm_in[i].gnt = arb_masters_gnt[i];
-        end else begin
-          arbiter_req_in[i] = 0;
-          tcdm_out[i].req = tcdm_in[i].req;
-          tcdm_in[i].gnt = tcdm_out[i].gnt;
-        end
+      // Forward the responses back to the correct requesters
+      if (outstanding_cache_response && i == arb_curr_idx) begin
+          tcdm_in[i].r_valid = arb_2_cache_rvalid;
+          tcdm_in[i].r_rdata = arb_2_cache_rdata;
+          tcdm_in[i].r_opc = arb_2_cache_err;
       end else begin
-        arbiter_req_in[i] = 0;
-        tcdm_out[i].req = 0;
-        tcdm_in[i].gnt = 0;
+          tcdm_in[i].r_valid = tcdm_out[i].r_valid;
+          tcdm_in[i].r_rdata = tcdm_out[i].r_rdata;
+          tcdm_in[i].r_opc = tcdm_out[i].r_opc;
       end
     end
   end
 
-
-  //Aggregated Request Data
-  logic [NR_MASTER_PORTS-1:0][REQ_AGG_DATA_WIDTH-1:0] req_data_agg_in;
-  logic [REQ_AGG_DATA_WIDTH-1:0] req_data_agg_out;
-  //Aggreagate the input data
-  for (genvar i = 0; i < NR_MASTER_PORTS; i++) begin
-    assign req_data_agg_in[i] = {tcdm_in[i].wen, tcdm_in[i].be, tcdm_in[i].add, tcdm_in[i].wdata};
-  end
-  //Disaggregate the output data
-  assign {arb_2_cache_wen, arb_2_cache_be, arb_2_cache_addr, arb_2_cache_wdata} = req_data_agg_out;
-
-  // index of the request chosen by the arbiter
-  logic [$clog2(NR_MASTER_PORTS)-1:0] arbiter_idx ;
-  // reg for previous index
-  reg [$clog2(NR_MASTER_PORTS)-1:0] last_idx ;
-
+  // Arbiter for access to the cache
   rr_arb_tree #(
     .NumIn     ( NR_MASTER_PORTS ),
     .DataWidth ( REQ_AGG_DATA_WIDTH ),
@@ -126,32 +101,32 @@ module pulpissimo_cache_subsystem_wrapper #(
     .rst_ni  ( rst_ni     ),
     .flush_i ( 1'b0       ),
     .rr_i    (            ),
+    .idx_o   ( arb_next_idx    ),
     // Masters <-> Arbiter
-    .req_i   ( arbiter_req_in ),
-    .gnt_o   ( arb_masters_gnt  ),
-    .data_i  ( req_data_agg_in ),
+    .req_i   ( req_2_arb_req  ),
+    .data_i  ( req_2_arb_data_agg ),
+    .gnt_o   ( req_2_arb_gnt  ),
     // Arbiter <-> Cache Subsystem
     .gnt_i   ( arb_2_cache_gnt   ),
     .req_o   ( arb_2_cache_req   ),
-    .data_o  ( req_data_agg_out ),
-    .idx_o   ( arbiter_idx )
+    .data_o  ( arb_2_cache_data_agg )
   );
 
-  // store the grant index
+  // Store the grant index of the arbiter
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
-      last_idx <= '0;
+      arb_curr_idx <= '0;
     end else begin
       if (arb_2_cache_req & arb_2_cache_gnt) begin
-        last_idx <= arbiter_idx;
+        arb_curr_idx <= arb_next_idx;
       end else begin
-        last_idx <= last_idx;
+        arb_curr_idx <= arb_curr_idx;
       end
     end
   end
 
-  // Create a separate signal for req and a obi pulp adapter, because req shall
-  // not be asserted while another request is being processed, I think
+  // The cache subsystem might issue another request before having received the
+  // response from the previous request, so we have to use an adapter
   logic tcdm_priv0_req;
   obi_pulp_adapter i_obi_pulp_adapter_data (
       .rst_ni         (rst_ni),
@@ -161,7 +136,8 @@ module pulpissimo_cache_subsystem_wrapper #(
       .mem_rvalid_i   (tcdm_priv0.r_valid),
       .mem_req_o      (tcdm_priv0.req)
   );
-  // Create the data cache. Connect it to the arbiter and the slave port
+
+  // Create the cache subsystem. Connect it to the arbiter and the priv0 output.
   pulpissimo_cache_subsystem cache_subsystem (
       .rst                (~rst_ni),
       .clk                (clk_i),
