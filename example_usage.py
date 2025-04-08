74f2d96bf4757f2b0a97cd57e8121879354c8f0c
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2025-04-08 10:57:17 +0200

    Updated example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -1,34 +1,35 @@
 from amaranth.back import verilog
 
-from pycachegen import (
-    ArbitrationScheme,
-    CacheConfig,
-    CacheWrapper,
-    ReplacementPolicies,
-    WritePolicies,
-)
+from pycachegen import *
 
 cache_wrapper = CacheWrapper(
-    address_width=8,
+    num_ports=1,
+    address_width=25,
     cache_configs=[
         CacheConfig(
-            data_width=16,
-            num_ways=1,
-            num_sets=4,
+            data_width=32,
+            num_ways=2,
+            num_sets=1,
+            block_size=2,
+            replacement_policy=ReplacementPolicies.PLRU_TREE,
+            write_policy=WritePolicies.WRITE_BACK,
+            write_allocate=True,
+            write_buffer_size=4,
+        ),
+        CacheConfig(
+            data_width=32,
+            num_ways=2,
+            num_sets=2,
+            block_size=4,
             replacement_policy=ReplacementPolicies.FIFO,
-            write_policy=WritePolicies.WRITE_THROUGH,
-            write_allocate=False,
-            block_size=1,
-        )
+            write_policy=WritePolicies.WRITE_BACK,
+            write_allocate=True,
+            write_buffer_size=8,
+        ),
     ],
-    main_memory_data_width=16,
-    create_main_memory=True,
-    num_ports=1,
-    read_delay=9,
-    write_delay=14,
-    arbitration_scheme=ArbitrationScheme.PRIORITY,
-    byte_size=8,
+    main_memory_data_width=128,
+    create_main_memory=False,
 )
 
 with open("cache_wrapper.v", "w") as f:
-    f.write(verilog.convert(cache_wrapper, name="CacheWrapper", emit_src=False))
+    f.write(verilog.convert(cache_wrapper, name="CacheWrapper"))
